da4c134ff3949cc8882d1430a6e8713bdfdf9bf9
tostenzel <tobias.stenzel@mailbox.org>
2020-03-31 15:32:54 +0200

    more flexbile testing with nox

--- noxfile.py
+++ noxfile.py
@@ -4,6 +4,10 @@ import nox
 
 @nox.session(python=["3.8", "3.7"])
 def tests(session):
+
+    # for running specific test files via nox
+    args = session.posargs or ["--cov"]
+
     session.install("poetry")
     session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov")
+    session.run("pytest", *args)
