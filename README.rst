f090df5e6625896691bcb69fbff9c7d72f0a00c0
tostenzel <tobias.stenzel@mailbox.org>
2020-04-01 20:48:12 +0200

    add codecov

--- README.rst
+++ README.rst
@@ -4,3 +4,6 @@ README
 
 .. image:: https://github.com/tostenzel/hypermodern-screening/workflows/Tests/badge.svg
   :target: https://github.com/tostenzel/hypermodern-screening/actions
+
+.. image:: https://codecov.io/gh/tostenzel/hypermodern-screening/branch/master/graph/badge.svg
+  :target: https://codecov.io/gh/tostenzel/hypermodern-screening
--- noxfile.py
+++ noxfile.py
@@ -145,3 +145,11 @@ def docs(session: Session) -> None:
     session.run("sphinx-build", "docs", "docs/_build")
 
 
+@nox.session(python="3.8")
+def coverage(session: Session) -> None:
+    """Upload coverage data."""
+    install_with_constraints(session, "coverage[toml]", "codecov")
+    session.run("coverage", "xml", "--fail-under=0")
+    session.run("codecov", *session.posargs)
+
+
